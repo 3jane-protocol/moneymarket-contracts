b6970270cbe763b52bff2b19213fc61f5ba2fd99
FP <fp_crypt@protonmail.com>
2025-08-18 11:54:36 -0700

    refactor: update storage slots and remove unused mappings

--- test/forge/libraries/periphery/MorphoStorageLibsVerificationTest.sol
+++ test/forge/libraries/periphery/MorphoStorageLibsVerificationTest.sol
@@ -8,19 +8,19 @@ import {IMorphoCredit} from "../../../../src/interfaces/IMorpho.sol";
 import "../../BaseTest.sol";
 
 /// @title MorphoStorageLibsVerificationTest
-/// @notice Comprehensive test to verify that all storage slot mappings in MorphoStorageLib 
+/// @notice Comprehensive test to verify that all storage slot mappings in MorphoStorageLib
 ///         and MorphoCreditStorageLib point to the correct actual storage locations
 contract MorphoStorageLibsVerificationTest is BaseTest {
     using MarketParamsLib for MarketParams;
 
     function setUp() public override {
         super.setUp();
-        
+
         // Market is already created in super.setUp(), no need to create again
-        
+
         // Set helper and usd3 for MorphoCredit testing if not already set
         IMorphoCredit morphoCredit = IMorphoCredit(address(morpho));
-        
+
         vm.startPrank(OWNER);
         if (morphoCredit.helper() == address(0)) {
             morphoCredit.setHelper(address(0x1234));
@@ -48,7 +48,7 @@ contract MorphoStorageLibsVerificationTest is BaseTest {
     function testMorphoStorageLib_NonceSlot() public {
         // First, increment nonce to have a non-zero value
         address testUser = address(0x9999);
-        
+
         // We need to trigger nonce increment through setAuthorizationWithSig or similar
         // For now, we'll just check the slot calculation is correct even with zero value
         uint256 expectedNonce = morpho.nonce(testUser);
@@ -57,62 +57,54 @@ contract MorphoStorageLibsVerificationTest is BaseTest {
         assertEq(actualNonce, expectedNonce, "MorphoStorageLib.nonceSlot() returns wrong slot");
     }
 
-    function testMorphoStorageLib_IsAuthorizedSlot_ShouldFail() public {
-        // This test should fail because isAuthorized mapping doesn't exist in Morpho.sol
-        // The library incorrectly assumes there's an isAuthorized mapping at slot 6
-        address authorizer = address(0x1111);
-        address authorizee = address(0x2222);
-        
-        // Try to use the isAuthorizedSlot function
-        bytes32 authSlot = MorphoStorageLib.isAuthorizedSlot(authorizer, authorizee);
-        
-        // This should not match any actual authorization data
-        // Since the mapping doesn't exist, reading from this slot should give unexpected results
-        bytes32 slotValue = vm.load(address(morpho), authSlot);
-        
-        // The slot should be empty or contain unrelated data
-        // This assertion will help identify that the slot mapping is wrong
-        assertEq(uint256(slotValue), 0, "isAuthorizedSlot points to non-existent mapping - should be empty");
-    }
-
     function testMorphoStorageLib_MarketSlots() public {
         Id id = marketParams.id();
-        
+
         // Supply some assets to have non-zero values in market
         loanToken.setBalance(address(this), 1000e18);
         morpho.supply(marketParams, 1000e18, 0, address(this), hex"");
-        
+
         // Test market total supply slot
         Market memory marketData = morpho.market(id);
         uint128 expectedTotalSupply = marketData.totalSupplyAssets;
         bytes32 marketSlot = MorphoStorageLib.marketTotalSupplyAssetsAndSharesSlot(id);
         uint128 actualTotalSupply = uint128(uint256(vm.load(address(morpho), marketSlot)));
-        assertEq(actualTotalSupply, expectedTotalSupply, "MorphoStorageLib.marketTotalSupplyAssetsAndSharesSlot() returns wrong slot");
-        
+        assertEq(
+            actualTotalSupply,
+            expectedTotalSupply,
+            "MorphoStorageLib.marketTotalSupplyAssetsAndSharesSlot() returns wrong slot"
+        );
+
         // Test market total borrow slot
         uint128 expectedTotalBorrow = marketData.totalBorrowAssets;
         bytes32 borrowSlot = MorphoStorageLib.marketTotalBorrowAssetsAndSharesSlot(id);
         uint128 actualTotalBorrow = uint128(uint256(vm.load(address(morpho), borrowSlot)));
-        assertEq(actualTotalBorrow, expectedTotalBorrow, "MorphoStorageLib.marketTotalBorrowAssetsAndSharesSlot() returns wrong slot");
-        
+        assertEq(
+            actualTotalBorrow,
+            expectedTotalBorrow,
+            "MorphoStorageLib.marketTotalBorrowAssetsAndSharesSlot() returns wrong slot"
+        );
+
         // Test market last update and fee slot
         uint128 expectedLastUpdate = marketData.lastUpdate;
         bytes32 updateSlot = MorphoStorageLib.marketLastUpdateAndFeeSlot(id);
         uint128 actualLastUpdate = uint128(uint256(vm.load(address(morpho), updateSlot)));
-        assertEq(actualLastUpdate, expectedLastUpdate, "MorphoStorageLib.marketLastUpdateAndFeeSlot() returns wrong slot");
+        assertEq(
+            actualLastUpdate, expectedLastUpdate, "MorphoStorageLib.marketLastUpdateAndFeeSlot() returns wrong slot"
+        );
     }
 
     function testMorphoStorageLib_PositionSlots() public {
         Id id = marketParams.id();
         address testUser = address(0x3333);
-        
+
         // Supply some assets to have non-zero position
         loanToken.setBalance(testUser, 500e18);
         vm.prank(testUser);
         loanToken.approve(address(morpho), 500e18);
         vm.prank(testUser);
         morpho.supply(marketParams, 500e18, 0, testUser, hex"");
-        
+
         // Test position supply shares slot
         uint256 expectedShares = morpho.position(id, testUser).supplyShares;
         bytes32 sharesSlot = MorphoStorageLib.positionSupplySharesSlot(id, testUser);
@@ -126,7 +118,7 @@ contract MorphoStorageLibsVerificationTest is BaseTest {
         bytes32 irmSlot = MorphoStorageLib.isIrmEnabledSlot(address(irm));
         bool actualIrmEnabled = uint256(vm.load(address(morpho), irmSlot)) != 0;
         assertEq(actualIrmEnabled, expectedIrmEnabled, "MorphoStorageLib.isIrmEnabledSlot() returns wrong slot");
-        
+
         // Test LLTV enabled slot
         bool expectedLltvEnabled = morpho.isLltvEnabled(DEFAULT_TEST_LLTV);
         bytes32 lltvSlot = MorphoStorageLib.isLltvEnabledSlot(DEFAULT_TEST_LLTV);
@@ -137,57 +129,61 @@ contract MorphoStorageLibsVerificationTest is BaseTest {
     function testMorphoStorageLib_IdToMarketParams_LoanToken() public {
         Id id = marketParams.id();
         MarketParams memory params = morpho.idToMarketParams(id);
-        
+
         address expectedLoanToken = params.loanToken;
         bytes32 loanTokenSlot = MorphoStorageLib.idToLoanTokenSlot(id);
         address actualLoanToken = address(uint160(uint256(vm.load(address(morpho), loanTokenSlot))));
         assertEq(actualLoanToken, expectedLoanToken, "MorphoStorageLib.idToLoanTokenSlot() returns wrong slot");
     }
-    
+
     function testMorphoStorageLib_IdToMarketParams_CollateralToken() public {
         Id id = marketParams.id();
         MarketParams memory params = morpho.idToMarketParams(id);
-        
+
         address expectedCollateralToken = params.collateralToken;
         bytes32 collateralTokenSlot = MorphoStorageLib.idToCollateralTokenSlot(id);
         address actualCollateralToken = address(uint160(uint256(vm.load(address(morpho), collateralTokenSlot))));
-        assertEq(actualCollateralToken, expectedCollateralToken, "MorphoStorageLib.idToCollateralTokenSlot() returns wrong slot");
+        assertEq(
+            actualCollateralToken,
+            expectedCollateralToken,
+            "MorphoStorageLib.idToCollateralTokenSlot() returns wrong slot"
+        );
     }
-    
+
     function testMorphoStorageLib_IdToMarketParams_Oracle() public {
         Id id = marketParams.id();
         MarketParams memory params = morpho.idToMarketParams(id);
-        
+
         address expectedOracle = params.oracle;
         bytes32 oracleSlot = MorphoStorageLib.idToOracleSlot(id);
         address actualOracle = address(uint160(uint256(vm.load(address(morpho), oracleSlot))));
         assertEq(actualOracle, expectedOracle, "MorphoStorageLib.idToOracleSlot() returns wrong slot");
     }
-    
+
     function testMorphoStorageLib_IdToMarketParams_IRM() public {
         Id id = marketParams.id();
         MarketParams memory params = morpho.idToMarketParams(id);
-        
+
         address expectedIrm = params.irm;
         bytes32 irmSlot = MorphoStorageLib.idToIrmSlot(id);
         address actualIrm = address(uint160(uint256(vm.load(address(morpho), irmSlot))));
         assertEq(actualIrm, expectedIrm, "MorphoStorageLib.idToIrmSlot() returns wrong slot");
     }
-    
+
     function testMorphoStorageLib_IdToMarketParams_LLTV() public {
         Id id = marketParams.id();
         MarketParams memory params = morpho.idToMarketParams(id);
-        
+
         uint256 expectedLltv = params.lltv;
         bytes32 lltvSlot = MorphoStorageLib.idToLltvSlot(id);
         uint256 actualLltv = uint256(vm.load(address(morpho), lltvSlot));
         assertEq(actualLltv, expectedLltv, "MorphoStorageLib.idToLltvSlot() returns wrong slot");
     }
-    
+
     function testMorphoStorageLib_IdToMarketParams_CreditLine() public {
         Id id = marketParams.id();
         MarketParams memory params = morpho.idToMarketParams(id);
-        
+
         address expectedCreditLine = params.creditLine;
         bytes32 creditLineSlot = MorphoStorageLib.idToCreditLineSlot(id);
         address actualCreditLine = address(uint160(uint256(vm.load(address(morpho), creditLineSlot))));
@@ -195,7 +191,6 @@ contract MorphoStorageLibsVerificationTest is BaseTest {
     }
 
     function testMorphoCreditStorageLib_HelperSlot() public {
-        // This test WILL FAIL because the library says slot 20 but it's actually at slot 19
         IMorphoCredit morphoCredit = IMorphoCredit(address(morpho));
         address expectedHelper = morphoCredit.helper();
         bytes32 helperSlot = MorphoCreditStorageLib.helperSlot();
@@ -203,25 +198,7 @@ contract MorphoStorageLibsVerificationTest is BaseTest {
         assertEq(actualHelper, expectedHelper, "MorphoCreditStorageLib.helperSlot() returns wrong slot");
     }
 
-    function testMorphoCreditStorageLib_ProtocolConfigSlot() public {
-        // This test WILL FAIL because protocolConfig is immutable (not in storage)
-        bytes32 protocolConfigSlot = MorphoCreditStorageLib.protocolConfigSlot();
-        
-        // Try to read from the slot the library claims
-        bytes32 slotValue = vm.load(address(morpho), protocolConfigSlot);
-        
-        // This should not contain the protocol config address since it's immutable
-        // We expect this to be zero or contain different data
-        IMorphoCredit morphoCredit = IMorphoCredit(address(morpho));
-        
-        // Get the actual protocolConfig value (immutable)
-        // Note: We can't directly compare since it's not in storage
-        // This test demonstrates the slot is wrong
-        assertEq(uint256(slotValue), 0, "protocolConfigSlot points to wrong location - protocolConfig is immutable");
-    }
-
     function testMorphoCreditStorageLib_Usd3Slot() public {
-        // This test WILL FAIL because the library says slot 22 but it's actually at slot 20
         IMorphoCredit morphoCredit = IMorphoCredit(address(morpho));
         address expectedUsd3 = morphoCredit.usd3();
         bytes32 usd3Slot = MorphoCreditStorageLib.usd3Slot();
@@ -230,67 +207,55 @@ contract MorphoStorageLibsVerificationTest is BaseTest {
     }
 
     function testMorphoCreditStorageLib_BorrowerPremiumSlot() public {
-        // This test WILL FAIL because the library uses wrong base slot (23 instead of 21)
         Id id = marketParams.id();
         address borrower = address(0x4444);
-        
-        // The slot calculation will be wrong due to incorrect base slot
+
         bytes32 premiumSlot = MorphoCreditStorageLib.borrowerPremiumSlot(id, borrower);
-        
-        // Read from the calculated slot
+
         bytes32 slotValue = vm.load(address(morpho), premiumSlot);
-        
-        // Since the base slot is wrong, this won't match actual premium data
-        // For now, we just verify the slot calculation itself runs
-        // The actual data mismatch will be evident when we have premium data set
+
         assertTrue(premiumSlot != bytes32(0), "Premium slot calculation should return non-zero");
     }
 
     function testMorphoCreditStorageLib_PaymentCycleSlots() public {
-        // This test WILL FAIL because the library uses wrong base slot (24 instead of 22)
         Id id = marketParams.id();
-        
-        // Test payment cycle length slot
+
         bytes32 lengthSlot = MorphoCreditStorageLib.paymentCycleLengthSlot(id);
-        
-        // Test payment cycle element slot
+
         bytes32 elementSlot = MorphoCreditStorageLib.paymentCycleElementSlot(id, 0);
-        
-        // These slots will be wrong due to incorrect base slot
+
         assertTrue(lengthSlot != bytes32(0), "Payment cycle length slot calculation should return non-zero");
         assertTrue(elementSlot != bytes32(0), "Payment cycle element slot calculation should return non-zero");
     }
 
     function testMorphoCreditStorageLib_RepaymentObligationSlot() public {
-        // This test WILL FAIL because the library uses wrong base slot (25 instead of 23)
         Id id = marketParams.id();
         address borrower = address(0x5555);
-        
+
         bytes32 obligationSlot = MorphoCreditStorageLib.repaymentObligationSlot(id, borrower);
-        
-        // The slot will be wrong due to incorrect base slot
+
         assertTrue(obligationSlot != bytes32(0), "Repayment obligation slot calculation should return non-zero");
     }
 
     function testMorphoCreditStorageLib_MarkdownStateSlot() public {
-        // This test WILL FAIL because the library uses wrong base slot (26 instead of 24)
         Id id = marketParams.id();
         address borrower = address(0x6666);
-        
+
         bytes32 markdownSlot = MorphoCreditStorageLib.markdownStateSlot(id, borrower);
-        
-        // The slot will be wrong due to incorrect base slot
+
         assertTrue(markdownSlot != bytes32(0), "Markdown state slot calculation should return non-zero");
     }
 
     function testMorphoCreditStorageLib_MarketTotalMarkdownAmountSlot() public {
         Id id = marketParams.id();
-        
+
         bytes32 markdownAmountSlot = MorphoCreditStorageLib.marketTotalMarkdownAmountSlot(id);
-        
+
         // This uses the market slot base + offset, verify it calculates correctly
-        assertTrue(markdownAmountSlot != bytes32(0), "Market total markdown amount slot calculation should return non-zero");
-        
+        assertTrue(
+            markdownAmountSlot != bytes32(0), "Market total markdown amount slot calculation should return non-zero"
+        );
+
         // Read the actual value (should be 0 initially)
         uint128 markdownAmount = uint128(uint256(vm.load(address(morpho), markdownAmountSlot)));
         assertEq(markdownAmount, 0, "Initial markdown amount should be zero");
